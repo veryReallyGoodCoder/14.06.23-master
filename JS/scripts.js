7722d8041cc7a54d3178f080405e89f688b38cc5
veryReallyGoodCoder <6000176@sae.edu.za>
2024-08-31 20:40:14 +0200

    GamePage || carousel added, index changes

---
+++ JS/scripts.js
@@ -0,0 +1,8 @@
+const myCarouselElement = document.querySelector('#carousel1')
+const carousel = new bootstrap.Carousel('#carousel1')
+
+
+// const carousel = new bootstrap.Carousel(myCarouselElement, {
+//   interval: 2000,
+//   touch: true
+// })
